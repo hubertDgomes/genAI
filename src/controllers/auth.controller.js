3d77e9d1ac9ae99cb7bfde5ddb360919a3754b3c
hubertDgomes <gomeshubert8@gmail.com>
2026-08-20 02:25:55 +0600

    chore: migrate from bcrypt to bcryptjs and update Vercel deployment configuration

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import userModel from "../models/user.model.js";
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import jwt from "jsonwebtoken";
 import 'dotenv/config'
 import tokenBlacklist from "../models/blacklist.model.js"
