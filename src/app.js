efc0fc17d5892b3d2a32d52f924a46a49d70a223
hubertDgomes <gomeshubert8@gmail.com>
2026-08-20 03:06:12 +0600

    refactor: remove redundant CORS preflight handler from app middleware

--- src/app.js
+++ src/app.js
@@ -17,7 +17,6 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
-app.options("*", cors())
 
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
